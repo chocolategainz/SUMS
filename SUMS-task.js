4639f364fffa9b86e33485f263ead74211f9d536
Aaron Thomas <05moreminutes@gmail.com>
2026-08-06 15:58:46 +0100

    Cleaning up HTML for final submission

--- SUMS-task.js
+++ SUMS-task.js
@@ -1,4 +1,4 @@
-//Event Handler
+//Event Handler for my 'Request a Demo' form
 function submitForm(event) {
 event.preventDefault(); 
 alert("Thank you for your request. A member of the SUMS team will be in touch shortly.")
